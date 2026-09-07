5b40b9b7dbc1760935077848ecaf9be5710d9173
adbermudez <adbermudez@unicesar.edu.co>
2026-09-07 10:28:33 -0500

    feat: Create atributes, Getter and Setters

--- src/main/java/com/gamezone/model/Cable.java
+++ src/main/java/com/gamezone/model/Cable.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.gamezone.model;
-
+  
 /**
- *
- * @author ASUS
+ * Represents a cable accesary
  */
-public class Cable {
+public class Cable extends Accesory {
+
+    // Atributos propios de Console
+    private float meters;
+    private String type;
+
+    /**
+     * Creates a new Accesory.
+     *
+     * @param id         unique identifier
+     * @param title      console title
+     * @param price      unit price
+     * @param quantity   quantity available in stock
+     * @param meters      Long in meters
+     * @param type      specific type of cable
+     */
+    public Cable(String id, String title, double price, int quantity,
+                    float meters, String type ) {
+        super(id, title, price, quantity);
+        this.meters = meters;
+        this.type = type;
+    }
+
+    public float getMeters() {
+        return meters;
+    }
+
+    public void setMeters(float meters) {
+        this.meters = meters;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     
-}
+
+    }
+
+
